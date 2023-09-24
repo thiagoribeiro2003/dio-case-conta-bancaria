3dbab742b85a6000f7d34c6d1f67c2e5cbdecb20
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-09-23 23:27:38 -0300

    Desenvolvimento do Case

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World");
+    public static void main (String[] args){
+        //TODO:Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+
+        Scanner meuScn = new Scanner(System.in).useLocale(Locale.US);
+
+        int numeroConta;
+        String numeroAgencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("BEM VINDO(A) AO BANCO XPTO");
+        System.out.println("VAMOS EFETUAR SEU SAQUE");
+        
+        System.out.println("--------------------------");
+
+        System.out.print("Digite seu nome: ");
+        nomeCliente = meuScn.nextLine();
+
+        System.out.print("Por favor, digite o número da sua Agência: ");
+        numeroAgencia = meuScn.nextLine();
+
+        System.out.print("Por favor, digite o número da sua Conta: ");
+        numeroConta = meuScn.nextInt();
+
+        System.out.print("Digite seu saldo: ");
+        saldo = meuScn.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já esta disponível para saque");
     }
 }
